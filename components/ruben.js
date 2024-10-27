b3ba556c9018f2d7052146a68d28a04fcddf22e6
jooseeruu <arjona2908@gmail.com>
2024-10-27 22:08:06 +0100

    NUMBER pasar revisión, validacion relativa

--- components/ruben.js
+++ components/ruben.js
@@ -1,5 +1,9 @@
 //STRINGS
 function formatearNombreConcierto(nombre) {
+  if (typeof nombre !== "string") {
+    console.log("Error, el valor debe ser de tipo string");
+    return false;
+  }
   let palabras = nombre.split(" "); // Divide el nombre en un array de palabras
   for (let i = 0; i < palabras.length; i++) {
     palabras[i] =
@@ -10,6 +14,10 @@ function formatearNombreConcierto(nombre) {
 }
 
 function formatearNombreArtista(artista) {
+  if (typeof artista !== "string") {
+    console.log("Error, el valor debe ser de tipo string");
+    return false;
+  }
   let artistaLimpio = artista.trim(); // Elimina espacios al principio y al final
   let palabras = artistaLimpio.split(" ");
   let palabrasLimpias = [];
@@ -19,13 +27,65 @@ function formatearNombreArtista(artista) {
       palabrasLimpias.push(palabra);
     }
   });
-
   return palabrasLimpias.join(" ");
 }
 
 function crearDescripcionEvento(nombreConcierto, nombreArtista, fecha) {
+  if (
+    typeof nombreConcierto !== "string" ||
+    typeof nombreArtista !== "string"
+  ) {
+    console.log("Error, los campos deben ser de tipo string");
+    return false;
+  }
+
   let conciertoFormateado = formatearNombreConcierto(nombreConcierto);
   let artistaFormateado = formatearNombreArtista(nombreArtista);
   let descripcion = `${conciertoFormateado} con ${artistaFormateado} el ${fecha}`;
   return descripcion;
+}
+
+//NUMBER
+function validarTicketsDisponibles(ticketsIngresados, maxTickets) {
+  if (typeof ticketsIngresados !== "number" || typeof maxTickets !== "number") {
+    console.log("Error, los campos deben ser números");
+    return false;
+  }
+
+  if (ticketsIngresados < 0 || maxTickets < 0) {
+    console.log("Error, los valores deben ser positivos");
+    return false;
+  }
+
+  return ticketsIngresados <= maxTickets;
+}
+
+function calcularIngresoAsistente(ingresoTotal, asistentes) {
+  if (typeof ingresoTotal !== "number" || typeof asistentes !== "number") {
+    console.log("Error, los campos deben ser números");
+    return false;
+  }
+
+  if (ingresoTotal < 0 || asistentes < 0) {
+    console.log("Error, los valores deben ser positivos");
+    return false;
+  }
+
+  return ingresoTotal / asistentes;
+}
+
+function generarIDConcierto(fechaConcierto, precioBase) {
+  // Reemplaza todos los guiones en la fecha con una cadena vacía, es decir, los elimina
+  let fechaFormateada = fechaConcierto.replace(/-/g, "");
+
+  // Convierte el precio a una cadena y elimina el punto decimal
+  let precioFormateado = precioBase.toString().replace(".", "");
+
+  // Genera dos valores aleatorios entre 00 y 99
+  let randomValue = Math.floor(Math.random() * 100)
+    .toString() // Pasar número a string
+    .padStart(2, "0"); // Se utiliza para asegurar que el número aleatorio tenga siempre 2 dígitos
+
+  // Concatena la fecha formateada, el precio formateado y el valor aleatorio
+  return fechaFormateada + precioFormateado + randomValue;
 }
